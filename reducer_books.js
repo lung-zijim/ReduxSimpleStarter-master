361c7e815256af61c26fc99eec30ff8d3844c95e
Ziyan Long <lzylong@gmail.com>
2018-03-01 11:19:29 -0800

    booklist

---
+++ reducer_books.js
@@ -0,0 +1,19 @@
+" Press ? for help
+
+.. (up a dir)
+/home/ava1on/Nextcloud/ReduxSimpleStarter-master/
+▾ src/
+  ▾ actions/
+      index.js
+  ▾ components/
+      app.js
+  ▾ reducers/
+      index.js
+    index.js
+▸ style/
+▸ test/
+  index.html
+  LICENSE.MD
+  package.json
+  README.md
+  webpack.config.js
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Java' },
+    { title: 'Horo' }, 
+    { title: 'Violet' }, 
+    { title: 'faceb' }
+  ]
+}
